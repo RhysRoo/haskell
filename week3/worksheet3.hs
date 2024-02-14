5fa21b5f4ca5af0db17b0170f883fa0a37e5f145
RhysRoo <93945223+RhysRoo@users.noreply.github.com>
2024-02-14 18:50:38 +0000

    progress:     git add .     git commit -m "progress"     git push

--- week3/worksheet3.hs
+++ week3/worksheet3.hs
@@ -1,4 +1,13 @@
 import Prelude hiding ((||),(&&))
+infixr 3 &&
+
+-- Example
+
+(||) :: Bool -> Bool -> Bool
+True || True = True
+False || True = True
+True || False = True
+False || False = False
 
 --Worked example
 nor :: Bool -> Bool -> Bool
@@ -10,3 +19,28 @@ nor True True = False
 nor2 :: Bool -> Bool -> Bool
 nor2 False x = not x
 nor2 True _ = False
+
+--Question 1
+(&&) :: Bool -> Bool -> Bool
+True && True = True
+_ && _ = False
+
+--Question 2
+exOr :: Bool -> Bool -> Bool
+exOr False False = False
+exOr False True  = True
+exOr True  False = True
+exOr True  True  = False
+
+--Question 3
+ifThenElse :: Bool -> Int -> Int -> Int
+ifThenElse True  x _ = x
+ifThenElse False _ y = y
+
+--Question 4
+
+
+
+
+
+
